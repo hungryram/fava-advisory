dfe01910fa4184998e2c07266fc719e3019654f0
Ram Dettmer <ramdettmer751@gmail.com>
2022-04-05 18:16:12 -0700

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,27 +6,29 @@ import * as Styles from "../styles/navbar.module.css"
 
 export default function Navbar() {
     return (
-        <div className="uk-position-top uk-padding uk-light">
+        <div className="uk-position-top uk-light">
             <div className="uk-navbar-container uk-navbar-transparent">
-                <div data-uk-navbar>
-                    <div className="uk-navbar-left">
-                        <Link to="/" className="uk-navbar-item uk-logo">
-                            {data.branding.logo ?
-                                <img src={data.branding.logo} alt="" /> : <h1 className="uk-h2">Fava Advisory</h1>
-                            }
-                        </Link>
-                        <ul className="uk-navbar-nav">
-                            {menu.menu.map((node) => {
-                                return (
-                                    <li>
-                                        <Link to={node.link}>{node.name}</Link>
-                                    </li>
-                                )
-                            })}
-                        </ul>
-                    </div>
-                    <div className="uk-navbar-right">
-                        <Link className="uk-button uk-button-primary">Search Homes</Link>
+                <div className="uk-container uk-container-expand">
+                    <div data-uk-navbar>
+                        <div className="uk-navbar-left">
+                            <Link to="/" className="uk-navbar-item uk-logo">
+                                {data.branding.logo ?
+                                    <img src={data.branding.logo} alt="" /> : <h1 className="uk-h2">Fava Advisory</h1>
+                                }
+                            </Link>
+                            <ul className="uk-navbar-nav">
+                                {menu.menu.map((node) => {
+                                    return (
+                                        <li>
+                                            <Link to={node.link}>{node.name}</Link>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+                        <div className="uk-navbar-right">
+                            <Link className="uk-button uk-button-primary">Search Homes</Link>
+                        </div>
                     </div>
                 </div>
             </div>
