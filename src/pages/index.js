27bee1075e7add7e7ae16fa877e112b4eea1c407
Ram Dettmer <ramdettmer751@gmail.com>
2022-04-05 18:13:38 -0700

    init

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Layout from "../components/Layout"
+import Hero from "../templates/home/Hero"
+
+export default function Home() {
+  return (
+    <Layout>
+      
+      <Hero />
+
+    </Layout>
+  )
+}
