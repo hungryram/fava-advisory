601ec96a40caa65f77d7089c27dc6f6baddf4bc9
Ram Dettmer <ramdettmer751@gmail.com>
2022-10-05 14:10:29 -0700

    add form to buildings

--- src/pages/buildings/{sanityBuildings.slug__current}.js
+++ src/pages/buildings/{sanityBuildings.slug__current}.js
@@ -1,10 +1,11 @@
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import * as React from "react"
 import Layout from "../../components/Layout"
 import ListingBanner from "../../components/templates/ListingBanner"
 import { PortableText } from "@portabletext/react"
 import Seo from "../../components/Seo"
 import { GatsbyImage } from "gatsby-plugin-image"
+import * as Styles from "../../styles/contact.module.css"
 
 export default function Building({ data }) {
     const frontMatter = data.sanityBuildings
@@ -130,6 +131,47 @@ export default function Building({ data }) {
                                     </div>
                                 )
                             })}
+                        </div>
+                    </div>
+                </div>
+                <div className="uk-section">
+                    <div className="uk-container">
+                        <h2>About Fava Advisory</h2>
+                        <p>Fava Advisory is a dynamic group of full-time real estate professionals who provide a highly personalized advisory service. We focus on curated real estate advice based on current marketing trends and creative marketing strategies. With over 30 years of combined experience in New York City and abroad, our business has grown through a referral network of repeat clients and we are well-equipped to handle all of your real estate needs. As downtown specialists, we have a true pulse on the market as it relates to your home, and you can feel confident in our ability to help you navigate the selling, buying, or renting process to maximize your goals.</p>
+                        <h2>Contact Us</h2>
+                        <p>Interested in a free, no-obligation consultation, we would love to set up a casual meet-up for coffee to discuss your plans and how we can be of service. Please complete the form below and we’ll reach out shortly!</p>
+                        <div className="uk-card uk-card-default uk-padding uk-margin-large-top">
+                            <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true" className={`uk-grid-small ${Styles.contactForm}`} data-uk-grid action="/thank-you">
+                                <input type="hidden" name="form-name" value="contact" />
+                                <input name="bot-field" type="hidden" />
+                                <input type="hidden" name="Subject" value="Website Contact Form" />
+                                <div className="uk-width-1-2@s">
+                                    <label htmlFor="fname">First Name*</label>
+                                    <input className="uk-input" type="text" name="First Name" id="fname" required />
+                                </div>
+                                <div className="uk-width-1-2@s">
+                                    <label htmlFor="lname">Last Name*</label>
+                                    <input className="uk-input" type="text" name="Last Name" id="lname" required />
+                                </div>
+                                <div className="uk-width-1-2@s">
+                                    <label htmlFor="phone">Phone Number (optional)</label>
+                                    <input className="uk-input" type="text" name="Phone" id="phone" />
+                                </div>
+                                <div className="uk-width-1-2@s">
+                                    <label htmlFor="email">Email Address*</label>
+                                    <input className="uk-input" type="text" name="Email" id="email" required />
+                                </div>
+                                <div className="uk-width-1-1">
+                                    <label htmlFor="email">Your Message*</label>
+                                    <textarea className="uk-textarea" rows="5" name="Message" id="message"></textarea>
+                                </div>
+                                <div className="uk-width-1-1">
+                                    <p className="uk-text-small">By submitting the form with your information above you are agreeing to our <Link to="/legal/terms-and-conditions">Terms and Conditions</Link> and <Link to="/legal/privacy-policy">Privacy Policy</Link></p>
+                                </div>
+                                <div className="uk-width-1-1">
+                                    <button className={Styles.submitBtn}>Submit</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
